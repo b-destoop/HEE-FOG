cc48461c66770d9e088850c38b9ed1416779fa6d
bert.destoop <bert.destoop@student.kuleuven.be>
2023-11-16 14:54:20 +0100

    implemented 50% PWM on GPIO5

--- main/actuate.c
+++ main/actuate.c
@@ -8,27 +8,56 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
+#include "driver/ledc.h"
 
-#define BLINK_GPIO 13
-#define AMOUNT_OF_BLINKS 5
 
-static uint8_t s_led_state = 0;
-static const char *TAG = "actuate";
+#define PWM_GPIO 13
 
-void blink_x_times(int amount) {
-    for (int i = 0; i < amount; i++) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        /* Set the GPIO level according to the state (LOW or HIGH)*/
-        gpio_set_level(BLINK_GPIO, s_led_state);
-        // Toggle the LED state //
-        s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
-    }
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
+#define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz
+
+
+static const char *TAG = "ACTUATE";
+
+static void pwm_init(void) {
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+            .speed_mode       = LEDC_MODE,
+            .timer_num        = LEDC_TIMER,
+            .duty_resolution  = LEDC_DUTY_RES,
+            .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 5 kHz
+            .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+            .speed_mode     = LEDC_MODE,
+            .channel        = LEDC_CHANNEL,
+            .timer_sel      = LEDC_TIMER,
+            .intr_type      = LEDC_INTR_DISABLE,
+            .gpio_num       = LEDC_OUTPUT_IO,
+            .duty           = 0, // Set duty to 0%
+            .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 }
 
 void actuate_main() {
     ESP_LOGI(TAG, "Main started");
-    blink_x_times(AMOUNT_OF_BLINKS);
+    pwm_init();
+
+    while (1) {
+        // Set duty to 50%
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+        // Update duty to apply the new value
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+    }
     ESP_LOGI(TAG, "Main ending...");
     vTaskDelete(NULL);
 }
