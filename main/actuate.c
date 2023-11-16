0bccad4cc97594c6f8e11b47a0d34c7dfed52c08
bert.destoop <bert.destoop@student.kuleuven.be>
2023-11-16 21:55:15 +0100

    link PWM to ADC input + drive PWM up to 2.5 MHz

--- main/actuate.c
+++ main/actuate.c
@@ -10,24 +10,24 @@
 #include "esp_log.h"
 #include "sdkconfig.h"
 #include "driver/ledc.h"
+#include "hal/ledc_types.h"
 
 // PWM DEFINES
 #define PWM_GPIO 13
 
+// pwm limit info:
+// https://docs.espressif.com/projects/esp-idf/en/v4.2/esp32/api-reference/peripherals/ledc.html#supported-range-of-frequency-and-duty-resolutions
 #define LEDC_TIMER              LEDC_TIMER_0
-#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_MODE               LEDC_HIGH_SPEED_MODE
 #define LEDC_OUTPUT_IO          (5) // Define the output GPIO
 #define LEDC_CHANNEL            LEDC_CHANNEL_0
-#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
-#define LEDC_DUTY               (4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
-#define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz
+#define LEDC_DUTY_RES           LEDC_TIMER_5_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095 / 8192
+#define LEDC_FREQUENCY          (2500000) // Frequency in Hertz. ==> higher frequency means smaller capacitor for RC filter
 
 // ADC DEFINES
 #define ADC_ATTEN           ADC_ATTEN_DB_11
 #define ADC_CHAN            ADC1_GPIO36_CHANNEL
-
-static int adc_raw[2][10];
-
 
 static const char *TAG = "ACTUATE";
 
@@ -36,10 +36,11 @@ static void pwm_init(void);
 
 void actuate_main() {
     ESP_LOGI(TAG, "Main started");
+
     pwm_init();
 
-
     //-------------ADC1 Init---------------//
+    static int adc_read;
     adc_oneshot_unit_handle_t adc1_handle;
     adc_oneshot_unit_init_cfg_t init_config1 = {
             .unit_id = ADC_UNIT_1,
@@ -54,14 +55,18 @@ void actuate_main() {
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHAN, &config));
 
     while (1) {
-        // Set duty to 50%
-        ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+        // ADC ONE-SHOT CODE
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHAN, &adc_read));
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_CHAN, adc_read);
+
+        // PWM CODE
+        // the adc is 12 bit, the duty cycle res is 4 bit ==> bit shift the adc reading 8 bits to the right
+        int duty = adc_read >> (12 - LEDC_DUTY_RES);
+        // Set duty where ADC_max=4095 and DUTY_max=8192 (=2^13)
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, duty));
         // Update duty to apply the new value
         ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
 
-        // ADC ONE-SHOT CODE
-        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHAN, &adc_raw[0][0]));
-        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_CHAN, adc_raw[0][0]);
 
         vTaskDelay(pdMS_TO_TICKS(1));
     }
