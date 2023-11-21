2fea0a71ee829a448046df2c14a89c310efd34e6
viktor dero <viktordero0@gmail.com>
2023-11-21 14:14:19 +0100

    FFT + bandbass added'

--- main/analyze.c
+++ main/analyze.c
@@ -1,11 +1,20 @@
 //“Difficult to see; always in motion is the future.” —Yoda, The Empire Strikes Back 
+
 #include <sys/cdefs.h>
 #include <stdio.h>
 #include "esp_log.h"
 #include "driver/i2c.h"
 #include "analyze.h"
+#include <time.h> 
+#include "FFT.h"
+//#include "FFT_signal.h"
+#include <math.h>
+//#include <FFTS.h> 
+
+
 
 static const char *TAG = "analyze";
+
 
 // Configuration the master be beeing a boss 
 esp_err_t Master_config(){
@@ -68,17 +77,108 @@ void wakeUpMPU6050() {
         vTaskDelay(10 / portTICK_PERIOD_MS);  // 10 ms coffee ;p
     }
 }
+//fft stufff=======================================================
+
+
+void fft(float data_re[], const unsigned int N)
+{
+  float dataIM[ARRAY_SIZE]= {0.0};
+  rearrange(data_re, dataIM, N);
+  compute(data_re, dataIM, N);
+}
+
+void rearrange(float data_re[], float data_im[], const unsigned int N)
+{
+  unsigned int target = 0;
+  for(unsigned int position=0; position<N; position++)
+  {
+    if(target>position) {
+      const float temp_re = data_re[target];
+      const float temp_im = data_im[target];
+      data_re[target] = data_re[position];
+      data_im[target] = data_im[position];
+      data_re[position] = temp_re;
+      data_im[position] = temp_im;
+    }
+    unsigned int mask = N;
+    while(target & (mask >>=1))
+      target &= ~mask;
+    target |= mask;
+  }
+}
+
+void compute(float data_re[], float data_im[], const unsigned int N)
+{
+  const float pi = -3.14159265358979323846;
+  
+  for(unsigned int step=1; step<N; step <<=1)
+  {
+    const unsigned int jump = step << 1;
+    const float step_d = (float) step;
+    float twiddle_re = 1.0;
+    float twiddle_im = 0.0;
+    for(unsigned int group=0; group<step; group++)
+    {
+      for(unsigned int pair=group; pair<N; pair+=jump)
+      {
+        const unsigned int match = pair + step;
+        const float product_re = twiddle_re*data_re[match]-twiddle_im*data_im[match];
+        const float product_im = twiddle_im*data_re[match]+twiddle_re*data_im[match];
+        data_re[match] = data_re[pair]-product_re;
+        data_im[match] = data_im[pair]-product_im;
+        data_re[pair] += product_re;
+        data_im[pair] += product_im;
+      }
+      
+      // we need the factors below for the next iteration
+      // if we don't iterate then don't compute
+      if(group+1 == step)
+      {
+        continue;
+      }
+
+      float angle = pi*((float) group+1)/step_d;
+      twiddle_re = cos(angle);
+      twiddle_im = sin(angle);
+    }
+  }
+}
+
+
+float findResonantFrequency(const float fftResult[], const int arraySize, float sampleRate) {
+    // Find the index with the maximum amplitude
+    int maxIndex = 0;
+    float maxAmplitude = 0.0;
+    float lowerCutoff = 1;
+    float  upperCutoff = 3; 
+
+
+    for (int i = 0; i < arraySize ; ++i) { 
+       float Frequency =  i* sampleRate / arraySize;
+        if (fftResult[i] > maxAmplitude && (Frequency > lowerCutoff || Frequency < upperCutoff)) {
+            maxAmplitude = fftResult[i];
+            maxIndex = i;
+        }
+    }
+
+    // Calculate the corresponding frequency in Hz
+    float resonantFrequency = (float)maxIndex * sampleRate / arraySize;
+
+    return resonantFrequency;
+}
+
+
+//fft stuff ===========================================================
 
 void analyze_main(){
-    ESP_LOGI(TAG, "Main started");
 
+    ESP_LOGI(TAG, "Main started");
     if (Master_config() != ESP_OK) {
         printf("I2C initialization failed\n");
         return;
     }
 
     wakeUpMPU6050();  // Wake up!!!
-    int ARRAY_SIZE = 50; // how many data elements you want in the array 
     uint8_t data[14]; // 14 bytes for 3 axes of accelerometer, 3 axes of gyroscope (we also got a temp sensor in there you know... xxx)
     float gyroscopeDataX[ARRAY_SIZE]; // array with gyro data of the X axis, initial list is filled with zero's
     float gyroscopeDataY[ARRAY_SIZE]; // array with gyro data of the Y axis, initial list is filled with zero's
@@ -87,8 +187,9 @@ void analyze_main(){
     float AccelerometerDataY[ARRAY_SIZE]; // array with gyro data of the Y axis, initial list is filled with zero's
     float AccelerometerDataZ[ARRAY_SIZE]; // you get the point don't you?
     int itterator = 0; 
-
+    
     while (1) {
+        
         if (i2c_master_read_slave(MPU6050_ADDR, 0x3B, data, sizeof(data)) == ESP_OK) {
             // now the same with the gyroscope 
             int16_t gyro_x = (data[0] << 8) | data[1];
@@ -119,7 +220,7 @@ void analyze_main(){
 
 
             // Print raw data values
-            //printf("Raw Data: X=%d, Y=%d, Z=%d\n", accel_x, accel_y, accel_z);
+            //printf("Raw Data: X=, Y=%d, Z=%d\n", accel_x, accel_y, accel_z);
 
             // Convert raw values to physical units (acceleration in g)
             float accel_x_g = accel_x;
@@ -131,15 +232,44 @@ void analyze_main(){
             float gyro_z_dps = gyro_z / 131.0;
 
             // Print the values for fun
-            printf("New measurment");
+            printf("New measurement");
             printf("Accelerometer: X=%.2f g, Y=%.2f g, Z=%.2f g\n", accel_x_g, accel_y_g, accel_z_g);
             printf("Gyroscope: X=%.2f dps, Y=%.2f dps, Z=%.2f dps\n", gyro_x_dps, gyro_y_dps, gyro_z_dps);
-        } else {
-            printf("Failed to read IMU's data\n");
+            
+            itterator = itterator + 1;
+            float listIM[ARRAY_SIZE] = {0};  // Initialize listIM with zeros
+            
+            if( itterator > ARRAY_SIZE){ 
+                itterator=0;
+                 printf("Non-FFT Data:\n");
+                for (unsigned int i = 0; i < ARRAY_SIZE; i++) {
+                    printf("%f ", AccelerometerDataX[i]);
+                }
+                printf("\n");
+            
+                fft(AccelerometerDataX, ARRAY_SIZE);
+                printf("FFT Data (Real Part):\n");
+                for (unsigned int i = 0; i < ARRAY_SIZE; i++) {
+                    printf("%f ", AccelerometerDataX[i]);
+                }
+                printf("\n");
+                float sampleRate = 1000.0 / TimeBeweenMeasurements; // Convert to seconds
+                float resonantFreq = findResonantFrequency(AccelerometerDataX, ARRAY_SIZE, sampleRate); 
+                printf("resonantfrequency= \n %f",resonantFreq);
+                printf("===================================================\n");
+
+            }
+            //if we speed this up we will do more measurments per s
+            vTaskDelay(TimeBeweenMeasurements  / portTICK_PERIOD_MS); // 4 measurments every second
         }
-        //if we speed this up we will do more measurments per s
-        vTaskDelay(250 / portTICK_PERIOD_MS); // 4 measurments every second
+        else {
+                printf("Failed to read IMU's data\n");
+        }
+        
+    
+         
     }
+    
 
     ESP_LOGI(TAG, "Main ending...");
 }
