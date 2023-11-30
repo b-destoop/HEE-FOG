0e8e32d1f36990bd09812c3d4b2a9aa45f1c72f5
bert.destoop <bert.destoop@student.kuleuven.be>
2023-11-30 01:29:58 +0100

    plot X_raw

--- data_vis.py
+++ data_vis.py
@@ -1,16 +1,25 @@
 import os
+import re
 import threading
+import time
 
+import pandas
 import serial.tools.list_ports
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import datetime as dt
+import queue
+import pandas as pd
 
 # serial info https://espressif-docs.readthedocs-hosted.com/projects/esp-idf/en/v3.3.5/get-started/establish-serial-connection.html
 # https://pyserial.readthedocs.io/en/latest/pyserial_api.html
 # -- CONFIG --
 PORT = ""
 BAUD_RATE = 115200
+
+MS_BETWEEN_READS = 10
+
+lines_queue = queue.Queue()
 
 
 def get_serial_data():
@@ -43,24 +52,38 @@ def get_serial_data():
 
         buffer += bytes
 
-        if buffer[-1] == "\n":
-            print(buffer[0:-2])
+        if buffer[0:2] != "ts":
             buffer = ""
+            continue
+
+        if buffer[-1] == "\n":
+            buffer = buffer[0:-2]  # trim off the \r and \n at the end of each line
+            # print(buffer)
+            lines_queue.put(buffer)
+            buffer = ""
+
+        time.sleep(MS_BETWEEN_READS / 1000)
 
     ser.close()
 
 
-def animate(i, ax, xs, ys):
-    # Read temperature (Celsius) from TMP102
-    value = 10
+dataframe = pd.DataFrame([])
 
-    # Add x and y to lists
-    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-    ys.append(value)
+
+def animate(i, ax):
+    global dataframe
+    # Read all data from queue
+    data_txt = lines_queue.get()
+
+    df_dict = serial_txt_to_dict(data_txt)
+
+    # pd.concat([dataframe, pd.DataFrame(df_dict, index=[0])])
+    dataframe = pd.concat([dataframe, pd.DataFrame(df_dict, index=[0])], ignore_index=True)
+    print(dataframe)
 
     # Limit x and y lists to 20 items
-    xs = xs[-100:]
-    ys = ys[-100:]
+    xs = dataframe["ts"][-100:]
+    ys = dataframe["X_raw"][-100:]
 
     # Draw x and y lists
     ax.clear()
@@ -69,29 +92,43 @@ def animate(i, ax, xs, ys):
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
-    plt.title('TMP102 Temperature over Time')
-    plt.ylabel('Temperature (deg C)')
+    plt.title('NOT TMP102 Temperature over Time')
+    plt.ylabel('NOT Temperature (deg C)')
+
+
+def serial_txt_to_dict(data_txt):
+    # Extracting variable names and values using regular expressions
+    matches = re.findall(r'(\w+)=([^;]+);', data_txt)
+    # adding variables and values in their respective columns in a pandas dataframe
+    df_dict = {}
+    for match in matches:
+        variable_name, value = match
+        try:
+            df_dict[variable_name] = float(value) if '[' not in value else print(value)
+        except ValueError as e:
+            print(e)
+            df_dict[variable_name] = 0
+
+    # print(df_dict)
+    return df_dict
 
 
 def handle_data():
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
-    xs = []
-    ys = []
+    dataframe = pd.DataFrame(serial_txt_to_dict(lines_queue.get()), index=[0])
 
     # Set up plot to call animate() function periodically
-    ani = animation.FuncAnimation(fig, animate, fargs=(ax, xs, ys), interval=1000)
+    ani = animation.FuncAnimation(fig, animate, fargs=(ax,), interval=100)
     plt.show()
+
     while True:
-        continue
+        data = lines_queue.get()
+        print(data)
 
 
-thread_serial = threading.Thread(target=get_serial_data, name="serial thread")
-thread_handle_data = threading.Thread(target=handle_data, name="handle data thread")
-
+thread_serial = threading.Thread(target=get_serial_data, name="serial thread", daemon=True)
 thread_serial.start()
-thread_handle_data.start()
-
+handle_data()
 thread_serial.join()
-thread_handle_data.join()
